0ef70780876bc49af54d9f15a281f1801aea606a
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2026-03-22 17:45:47 +0800

    feat(datafame): implement quicksort

--- include/datatypes/dataframe/ops/sort.h
+++ include/datatypes/dataframe/ops/sort.h
@@ -35,7 +35,6 @@ typedef enum {
 int df_sort(
     dataframe_t *df, 
     const char *col, 
-    const size_t n_cols, 
     const df_sort_order order
 );
 
--- test/datatypes/dataframe/ops/sort.c
+++ test/datatypes/dataframe/ops/sort.c
@@ -1,3 +1,18 @@
+#include "test_utils.h"
+#include "datatypes/dataframe/ops/sort.h"
+
 int main() {
-    // to be implemented
+    dataframe_t *df = generate_dummy_df(1000);
+
+    assert(df_sort(df, "foo", DF_SORT_ASC) == DF_OK);
+    for (size_t r = 0; r < 1000; r++)
+        assert(df->data[r * df->n_cols] == r + 1);
+
+    assert(df_sort(df, "foo", DF_SORT_DESC) == DF_OK);
+    for (size_t r = 0; r > 1000; r++)
+        assert(df->data[r * df->n_cols] == 999 - r);
+
+    assert(df_sort(df, "qux", DF_SORT_ASC) == DF_NO_COL);
+
+    df_free(df);
 }
